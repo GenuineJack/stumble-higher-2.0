ba430bb725807fc6716e6e773a5be4f9d0de512c
GenuineJack <6517070+GenuineJack@users.noreply.github.com>
2025-06-03 16:30:09 -0400

    Add files via upload

---
+++ api/proxy.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res){
+  const url = req.query.u;
+  if(!url) return res.status(400).send('Missing url');
+  try{
+    const r = await fetch(url, { headers:{ 'user-agent':'Mozilla/5.0'} });
+    const html = await r.text();
+    res.setHeader('Content-Type','text/html');
+    res.send(html);
+  }catch(e){
+    res.status(500).send('Proxy error');
+  }
+}
